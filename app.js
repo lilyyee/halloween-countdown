92a6ceea6bb800dfa62ea50d65f4dd4210c723ab
lilyyee <emaillilyyee@gmail.com>
2021-10-28 18:00:18 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,79 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const weekdays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const announce = document.querySelector('.announce');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2021, 9, 31, 12, 00, 0);
+
+const weekday = weekdays[futureDate.getDay()];
+const month = months[futureDate.getMonth()];
+const date = futureDate.getDate();
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes().toString().padStart(2, '0');
+
+announce.textContent = `Prepare to get scared on ${weekday}, ${month} ${date}, ${year} at ${hours}:${minutes}am.`;
+
+// future time in milliseconds
+const futureTime = futureDate.getTime();
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+
+  // values in milliseconds
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  // calculate all values
+  let days = Math.floor(t / oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
+
+  // set values array
+  const values = [days, hours, minutes, seconds];
+
+  function format(item) {
+    if (item < 10) {
+      return (item = `0${item}`);
+    }
+    return item;
+  }
+
+  items.forEach(function (item, index) {
+    item.innerHTML = format(values[index]);
+  });
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h3 class="expired">Halloween is here already! Boo!</h3>`;
+  }
+}
+
+// countdown
+let countdown = setInterval(getRemainingTime, 1000);
+getRemainingTime();
